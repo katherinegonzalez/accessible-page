a040c4e8a3233d2ebd5b76da328a3aba04b0471b
katherinex <kathexio@gmail.com>
2020-09-13 19:12:08 -0500

    Implement accessibility in men bar: move with arrows between menu items with Focus key manager

--- src/app/modules/core/components/menu-list-item/menu-list-item.component.ts
+++ src/app/modules/core/components/menu-list-item/menu-list-item.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit, HostBinding, ElementRef } from '@angular/core';
+import { Component, OnInit, HostBinding, ElementRef, Input } from '@angular/core';
 import { FocusableOption } from '@angular/cdk/a11y';
 
 @Component({
   selector: 'app-menu-list-item',
-  // templateUrl: './menu-list-item.component.html',
+  templateUrl: './menu-list-item.component.html',
   styleUrls: ['./menu-list-item.component.scss'],
-  template: '<ng-content></ng-content>'
+  // template: '<ng-content></ng-content>'
 })
 export class MenuListItemComponent implements OnInit, FocusableOption {
 
-  @HostBinding('attr.tabindex') tabindex = -1 ;
+  // @HostBinding('attr.tabindex') tabindex = -1 ;
+
+  @Input() fragment: string;
 
   constructor(public host: ElementRef) { }
 
@@ -17,7 +19,8 @@ export class MenuListItemComponent implements OnInit, FocusableOption {
   }
 
   focus() {
-    this.host.nativeElement.focus();
+    // this.host.nativeElement.focus();
+    this.host.nativeElement.children[0].children[0].focus(); // to make the focus in the a tag
   }
 
 }
--- src/app/modules/core/components/menu-list/menu-list.component.ts
+++ src/app/modules/core/components/menu-list/menu-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterContentInit, QueryList, ContentChildren } from '@angular/core';
+import { Component, OnInit, AfterContentInit, QueryList, ContentChildren, HostBinding } from '@angular/core';
 import { FocusKeyManager } from '@angular/cdk/a11y';
 import { MenuListItemComponent } from '../menu-list-item/menu-list-item.component';
 import { UP_ARROW, ESCAPE, hasModifierKey, LEFT_ARROW, RIGHT_ARROW, HOME, END, DOWN_ARROW, ENTER } from '@angular/cdk/keycodes';
@@ -10,12 +10,9 @@ import { UP_ARROW, ESCAPE, hasModifierKey, LEFT_ARROW, RIGHT_ARROW, HOME, END, D
 })
 export class MenuListComponent implements OnInit, AfterContentInit {
 
-  tabindex: number = null;
   _keyManager: FocusKeyManager<MenuListItemComponent>;
 
   @ContentChildren(MenuListItemComponent) menuItems: QueryList<MenuListItemComponent>;
-
-
 
   constructor() { }
 
@@ -23,16 +20,30 @@ export class MenuListComponent implements OnInit, AfterContentInit {
   }
 
   ngAfterContentInit(): void {
-    this._keyManager = new FocusKeyManager<MenuListItemComponent>(this.menuItems).withWrap();
-    if (this.menuItems.length > 0) {
-      this.tabindex = 0;
-    } else {
-      this.tabindex = -1;
-    }
+    this._keyManager = new FocusKeyManager<MenuListItemComponent>(this.menuItems).withWrap().withHorizontalOrientation('ltr');
   }
 
   handleKeydown(event: KeyboardEvent) {
-    this._keyManager.onKeydown(event);
+    const key = event.key;
+    switch (key) {
+      case 'ArrowRight':
+      case 'ArrowLeft':
+      case 'ArrowDown':
+      case 'ArrowUp':
+        this._keyManager.onKeydown(event);
+        break;
+      case 'Home':
+        this._keyManager.setFirstItemActive();
+        event.preventDefault();
+        break;
+      case 'End':
+        this._keyManager.setLastItemActive();
+        event.preventDefault();
+        break;
+      default:
+        this._keyManager.onKeydown(event);
+    }
+
   }
 
   /*handleKeydown(event: KeyboardEvent) {
